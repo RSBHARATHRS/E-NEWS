da33c73403d9340e6e53a7ade54b5726bf84bf4e
Bharath R <78311020+RSBHARATHRS@users.noreply.github.com>
2022-04-09 13:43:28 +0530

    login validation done

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
-import { FormControl, FormGroup, MinLengthValidator, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -8,30 +8,43 @@ import { FormControl, FormGroup, MinLengthValidator, Validators } from '@angular
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-
-  loginForm = new FormGroup({
-    'userName': new FormControl('', [Validators.required, Validators.minLength(4)]),
-    'password': new FormControl('', [Validators.required, Validators.minLength(4)]),
-  })
-
-  constructor(
-    public modalCtrl: ModalController,
-  ) {
-    this.loginForm;
-  }
+  loginForm: FormGroup;
+  isFormSubmitted: boolean = false
+  constructor(public modalCtrl: ModalController, private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.login()
   }
 
   login() {
-
+    this.loginForm = this.formBuilder.group({
+      userName: ['', [Validators.required, Validators.maxLength(40), this.emailValidation]],
+      password: ['', [Validators.required, Validators.minLength(8)]]
+    })
   }
-
+  get form() { return this.loginForm.controls; }
   forgotPassword() {
 
   }
-
+  emailValidation(control: AbstractControl) {
+    const regex = new RegExp(/^\s*\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+\s*$/);
+    if (regex.test(control.value)) {
+      return null
+    } else {
+      return {
+        emailValid: true
+      }
+    }
+  }
   async dismiss() {
     await this.modalCtrl.dismiss();
+  }
+  formSubmit() {
+    this.isFormSubmitted = true;
+    if (this.loginForm.invalid) {
+      return false;
+    } else {
+      console.log("form Submitted")
+    }
   }
 }
