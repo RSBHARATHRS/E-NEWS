d62331857b1176053fd730361d538c5ed53a6866
Bharath R <78311020+RSBHARATHRS@users.noreply.github.com>
2022-04-09 10:41:42 +0530

    Home page only added

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { NavController, PopoverController } from '@ionic/angular';
+import { NotificationsPage } from '../common/notifications/notifications';
+import { WeatherService } from '../services/weather.service';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +10,57 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  public search = {
+    name: "Rio de Janeiro, Brazil",
+    date: new Date().toISOString()
+  }
+
+  constructor(public nav: NavController, public popoverCtrl: PopoverController,
+    private weatherService: WeatherService) {
+  }
+
+  ionViewWillEnter() {
+    // this.search.pickup = "Rio de Janeiro, Brazil";
+    // this.search.dropOff = "Same as pickup";
+    // this.storage.get('pickup').then((val) => {
+    //   if (val === null) {
+    //     this.search.name = "Rio de Janeiro, Brazil"
+    //   } else {
+    //     this.search.name = val;
+    //   }
+    // }).catch((err) => {
+    //   console.log(err)
+    // });
+    console.log(this.weatherService.getWeather('tamilnadu', 'tirunelveli').subscribe());
+
+  }
+
+  // go to result page
+  doSearch() {
+    //this.nav.push(TripsPage);
+  }
+
+  // choose place
+  choosePlace(from) {
+    //this.nav.push(SearchLocationPage, from);
+  }
+
+  // to go account page
+  goToAccount() {
+    //this.nav.push(SettingsPage);
+  }
+
+  async presentNotifications(ev: any) {
+    const popover = await this.popoverCtrl.create({
+      component: NotificationsPage,
+      cssClass: 'my-custom-class',
+      event: ev,
+      translucent: true
+    });
+    await popover.present();
+
+    const { role } = await popover.onDidDismiss();
+    console.log('onDidDismiss resolved with role', role);
+  }
 
 }
