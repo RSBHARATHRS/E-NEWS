0dba178394d48e1fc3554e067a2e9ea2051d4fab
RSBHARATHRS <78311020+RSBHARATHRS@users.noreply.github.com>
2022-05-07 11:44:09 +0530

    firebase imports done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,14 +11,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 import { TextToSpeech } from '@ionic-native/text-to-speech/ngx';
+
 import { environment } from 'src/environments/environment';
+import { AngularFireModule } from "@angular/fire/compat";
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
 
 @NgModule({
     declarations: [AppComponent],
     imports: [BrowserModule,
         HttpClientModule,
         ReactiveFormsModule,
-       // AngularFireModule.initializeApp(environment.firebaseConfig),
+        AngularFireModule.initializeApp(environment.firebaseConfig),
+        AngularFireStorageModule,
+        AngularFirestoreModule,
+        AngularFireDatabaseModule,
         IonicModule.forRoot(),
         AppRoutingModule],
     providers: [
